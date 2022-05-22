ac8072c6e8ddf8670ecfb21d69146d4aa6992558
Luka Pavlović <luka.pavlovic@fer.hr>
2022-05-22 13:04:52 +0200

    fix for single language, when loading all languages use predefined parameter, else pass str of single language or list of multiple languages

--- MultiEvalDataset.py
+++ MultiEvalDataset.py
@@ -5,13 +5,17 @@ import re
 
 
 class MultiEurlexDataset(Dataset):
-    def __init__(self, split='train', language='all_languages'):
-        dataset = load_dataset('multi_eurlex', language)
+    def __init__(self, split='train', languages=[]):
+        dataset = load_dataset('multi_eurlex', 'all_languages')
         dataset_dict = {'celex_id': [],
                         'lang': [],
                         'document': [],
                         'labels': []
                         }
+
+        if isinstance(languages, str):
+            languages = [languages]
+            print(languages)
 
         regex1 = re.compile('[0-9\n/()\[\]\':;"\„\“\-»«\’\’\‘\”]')
         regex2 = re.compile('[\ ]+')
@@ -23,19 +27,20 @@ class MultiEurlexDataset(Dataset):
                 percentage = idx / len(dataset[split]) * 100
                 print(f'{percentage:.4f}% of dataset loaded')
             for lang in instance['text'].keys():
-                if instance['text'][lang]:
-                    instance['text'][lang] = regex1.sub('', instance['text'][lang])
-                    instance['text'][lang] = regex2.sub(' ', instance['text'][lang])
-                    instance['text'][lang] = regex3.sub('.', instance['text'][lang])
+                if not len(languages) or lang in languages:
+                    if instance['text'][lang]:
+                        instance['text'][lang] = regex1.sub('', instance['text'][lang])
+                        instance['text'][lang] = regex2.sub(' ', instance['text'][lang])
+                        instance['text'][lang] = regex3.sub('.', instance['text'][lang])
 
-                    dataset_dict['celex_id'].append(instance['celex_id'])
-                    dataset_dict['lang'].append(lang)
-                    dataset_dict['labels'].append(instance['labels'])
-                    dataset_dict['document'].append(instance['text'][lang])
+                        dataset_dict['celex_id'].append(instance['celex_id'])
+                        dataset_dict['lang'].append(lang)
+                        dataset_dict['labels'].append(instance['labels'])
+                        dataset_dict['document'].append(instance['text'][lang])
 
         flat_dataset = pd.DataFrame.from_dict(dataset_dict)
         self.data = flat_dataset
-        self.language = language
+        self.languages = languages
 
     def __len__(self):
         return len(self.data)
