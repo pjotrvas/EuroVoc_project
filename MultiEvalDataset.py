71475b2598755aa69c98fc046a253ee106afe7bb
vasiljmatea <matea.vasilj@fer.hr>
2022-05-22 12:22:20 +0200

    dataset cleanup

--- MultiEvalDataset.py
+++ MultiEvalDataset.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 from torch.utils.data import Dataset
 import pandas as pd
-
+import re
 
 class MultiEurlexDataset(Dataset):
     def __init__(self,split='train',language='all_languages'):
@@ -12,6 +12,10 @@ class MultiEurlexDataset(Dataset):
                 'labels': []
                 }
 
+        regex1 = re.compile('[0-9\n/()\[\]\':;"\„\“\-»«\’\’\‘\”]')
+        regex2 = re.compile('[\ ]+')
+        regex3 = re.compile('\ \.')
+
         for idx, instance in enumerate(dataset[split]):
             step = int(len(dataset[split])  / 10)
             if idx % step == 0:
@@ -19,6 +23,10 @@ class MultiEurlexDataset(Dataset):
                 print(f'{percentage:.4f}% of dataset loaded')
             for lang in instance['text'].keys():
                 if instance['text'][lang]:
+                    instance['text'][lang] = regex1.sub('', instance['text'][lang])
+                    instance['text'][lang] = regex2.sub(' ', instance['text'][lang])
+                    instance['text'][lang] = regex3.sub('.', instance['text'][lang])
+            
                     dataset_dict['celex_id'].append(instance['celex_id'])
                     dataset_dict['lang'].append(lang)
                     dataset_dict['labels'].append(instance['labels'])
